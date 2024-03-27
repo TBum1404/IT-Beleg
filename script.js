e4c98c6921234d4eb4dd22c70ba1bb64748a97f6
TBum1404 <tomraik1404@gmail.com>
2024-03-27 20:51:46 +0100

    funktionierende script einblendung

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ document.getElementById('datumButton').addEventListener('click', function() {
     var datum = new Date();
     document.getElementById('datumAnzeige').textContent = datum;
 });
+document.getElementById('nachrichtButton').addEventListener('click', function() {
+    alert('Hallo, dies ist eine Nachricht!');
+});
