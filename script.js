9c2eefd036b1ea95b88a91a71d6ad16f69257092
sWadenpresse120kg <tomraik1404@gmail.com>
2024-04-12 17:29:43 +0200

    wqer

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+
+document.getElementById('nachrichtButton').addEventListener('click', function() {
+    //alert('Hallo, dies ist eine Nachricht!');
+    var text = new Date();
+    document.getElementById('nachrichtAnzeige').textContent = 'moin mon';
+});
+
 document.getElementById('datumButton').addEventListener('click', function() {
     var datum = new Date();
     document.getElementById('datumAnzeige').textContent = datum;
-});
-document.getElementById('nachrichtButton').addEventListener('click', function() {
-    alert('Hallo, dies ist eine Nachricht!');
 });
